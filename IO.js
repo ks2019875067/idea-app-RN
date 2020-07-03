4284df7264f25b4d036ab8d38abbe470a89498b3
ks2019875067 <48702265+ks2019875067@users.noreply.github.com>
2020-07-03 17:31:05 +0900

    Add files via upload

---
+++ IO.js
@@ -0,0 +1,68 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import {Text, View, AsyncStorage } from 'react-native';
+
+class IO extends React.Component {
+
+static input = (value) => {
+  try{
+    key = Math.random();
+    AsyncStorage.setItem(key, JSON.stringify(value));
+    return key;
+  }
+  catch(error){
+    return -1;
+  }
+};
+static edit = (key) => {
+  try{
+    const value = AsyncStorage.getItem(key);
+    value = JSON.parse(value);
+    value =  Addscreen(value);
+    AsyncStorage.mergeItem(key, JSON.stringify(this.value));
+    return 1;
+  }
+  catch(error){
+    return -1;
+  }
+};
+static delete = (key) => {
+  try{
+    AsyncStorage.removeItem(key);
+    return 1;
+  }
+  catch(error){
+    return -1;
+  }
+};
+static tag = (keys) => {
+  try{
+    AsyncStorage.removeItem(key);
+    return 1;
+  }
+  catch(error){
+    return -1;
+  }
+};
+/*ab ={
+  name: 'chris',
+  age: 30,
+  traits: 'brown'
+};
+bc = {
+  name: 'fame',
+  age: 29,
+  traits: '20'
+};
+
+  render(){
+    const inkey = App.input(this.ab);
+
+    return (
+      <View>
+        <Text>{ inkey }, { App.delete(this.inkey)}, {App.AsyncStorage}</Text>
+      </View>
+
+    );
+  }
+}*/
